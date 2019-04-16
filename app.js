f7f795bd4373aa94b8f4b6a57f8bbb2a36a6715b
ariebrainware <publicu001@gmail.com>
2019-04-17 02:05:24 +0700

    Implement CORS

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ const express = require('express')
 const path = require('path')
 const cookieParser = require('cookie-parser')
 const logger = require('morgan')
+const cors = require('cors')
 
 const indexRouter = require('./routes/index')
 const topicRouter = require('./routes/topic')
 
 const app = express()
 
+app.use(cors())
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
